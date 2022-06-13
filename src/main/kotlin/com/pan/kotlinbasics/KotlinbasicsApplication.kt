05e324cd1fc5d8e083a6ad1faaaced9b0b7bc6ba
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-06-13 10:43:51 -0300

    Project Started

---
+++ src/main/kotlin/com/pan/kotlinbasics/KotlinbasicsApplication.kt
@@ -0,0 +1,11 @@
+package com.pan.kotlinbasics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinbasicsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinbasicsApplication>(*args)
+}
---
+++ src/test/kotlin/com/pan/kotlinbasics/KotlinbasicsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pan.kotlinbasics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinbasicsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
